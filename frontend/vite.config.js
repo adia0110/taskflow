796623f1814e916608d51250772b5f4a1a309ba9
Aditya <adityaagarwal151@gmail.com>
2026-05-02 12:32:06 +0530

    fix: CustomEvent issue for Railway build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+if (typeof global !== "undefined" && typeof global.CustomEvent === "undefined") {
+  global.CustomEvent = function () {};
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
